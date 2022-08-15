da2ea74737d2f3a0985f95cdc8043036248c883f
unknown <artemesia2009@gmail.com>
2022-08-14 20:43:31 -0400

    add a simple maven codes

---
+++ src/main/java/simplemaven/TestMaven.java
@@ -0,0 +1,10 @@
+package simplemaven;
+
+public class TestMaven {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test Hello World");
+	}
+
+}
